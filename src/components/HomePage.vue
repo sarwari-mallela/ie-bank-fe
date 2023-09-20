8a02e0a154ebe716a3a45b4da742135977bf9cec
sarwari-mallela <123460981+sarwari-mallela@users.noreply.github.com>
2023-09-20 19:51:52 +0200

    git commit -m "Added HomePage.vue and updated routing for the welcome page."

---
+++ src/components/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="home-page">
+      <div class="welcome-text">
+        <h1 class="pretty-font">Welcome to IE Bank</h1>
+        <p>Your Trusted Banking Partner</p>
+        <button class="action-button">View/Create Accounts</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Your component logic here
+  }
+  </script>
+  
+  <style scoped>
+  /* Styles for the HomePage.vue component */
+  
+  .home-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #ffc0cb; /* Baby Pink Background */
+  }
+  
+  .welcome-text {
+    text-align: center;
+    color: #fff; /* White Text Color */
+    padding: 20px;
+  }
+  
+  .pretty-font {
+    font-family: 'Cursive', sans-serif; /* Choose an appropriate pretty font */
+    font-size: 3rem;
+  }
+  
+  .action-button {
+    background-color: #ff69b47f; /* Hot Pink Button Color */
+    color: #fff; /* White Text Color */
+    border: none;
+    padding: 10px 20px;
+    font-size: 1.2rem;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+  
+  .action-button:hover {
+    background-color: #ff149147; /* Darker Pink on Hover */
+  }
+  </style>
+  
