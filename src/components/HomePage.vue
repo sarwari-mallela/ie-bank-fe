74c6a72ecc0671b9119d10d75e0bfc77c451babb
sarwari-mallela <123460981+sarwari-mallela@users.noreply.github.com>
2023-09-27 23:57:57 +0200

    Going from Home Page to Accounts page successfully done!

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -3,15 +3,19 @@
       <div class="welcome-text">
         <h1 class="pretty-font">Welcome to IE Bank</h1>
         <p>Your Trusted Banking Partner</p>
-        <button class="action-button">View/Create Accounts</button>
+        <button @click="navigateToAccounts" style = "background-color: rgba(218, 130, 177, 0.521); color:white;">View/Create Accounts</button>
       </div>
     </div>
   </template>
   
   <script>
   export default {
-    // Your component logic here
-  }
+    methods: {
+      navigateToAccounts() {
+        this.$router.push('/accounts');
+      },
+    },
+  };
   </script>
   
   <style scoped>
---
+++ src/components/__VLS_template.js
@@ -0,0 +1,93 @@
+import { __VLS_internalComponent, __VLS_componentsOption, __VLS_name } from './HomePage.vue';
+
+function __VLS_template() {
+let __VLS_ctx!: InstanceType<__VLS_PickNotAny<typeof __VLS_internalComponent, new () => {}>> & {};
+/* Components */
+let __VLS_otherComponents!: NonNullable<typeof __VLS_internalComponent extends { components: infer C; } ? C : {}> & typeof __VLS_componentsOption;
+let __VLS_own!: __VLS_SelfComponent<typeof __VLS_name, typeof __VLS_internalComponent & (new () => { $scopedSlots: typeof __VLS_slots; })>;
+let __VLS_localComponents!: typeof __VLS_otherComponents & Omit<typeof __VLS_own, keyof typeof __VLS_otherComponents>;
+let __VLS_components!: typeof __VLS_localComponents & __VLS_GlobalComponents & typeof __VLS_ctx;
+/* Style Scoped */
+type __VLS_StyleScopedClasses = {} &
+{ 'home-page'?: boolean; } &
+{ 'welcome-text'?: boolean; } &
+{ 'pretty-font'?: boolean; } &
+{ 'action-button'?: boolean; } &
+{ 'action-button'?: boolean; };
+let __VLS_styleScopedClasses!: __VLS_StyleScopedClasses | keyof __VLS_StyleScopedClasses | (keyof __VLS_StyleScopedClasses)[];
+/* CSS variable injection */
+/* CSS variable injection end */
+let __VLS_resolvedLocalAndGlobalComponents!: {};
+({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div; ({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).h1; ({} as __VLS_IntrinsicElements).h1;
+({} as __VLS_IntrinsicElements).p; ({} as __VLS_IntrinsicElements).p;
+({} as __VLS_IntrinsicElements).button; ({} as __VLS_IntrinsicElements).button;
+{
+const __VLS_0 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_1 = __VLS_elementAsFunctionalComponent(__VLS_0);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_2 = __VLS_1({ ...{}, class: ("home-page"), }, ...__VLS_functionalComponentArgsRest(__VLS_1));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_0, typeof __VLS_2> & Record<string, unknown>) => void)({ ...{}, class: ("home-page"), });
+const __VLS_3 = __VLS_pickFunctionalComponentCtx(__VLS_0, __VLS_2)!;
+let __VLS_4!: __VLS_NormalizeEmits<typeof __VLS_3.emit>;
+{
+const __VLS_5 = ({} as __VLS_IntrinsicElements)["div"];
+const __VLS_6 = __VLS_elementAsFunctionalComponent(__VLS_5);
+({} as __VLS_IntrinsicElements).div;
+({} as __VLS_IntrinsicElements).div;
+const __VLS_7 = __VLS_6({ ...{}, class: ("welcome-text"), }, ...__VLS_functionalComponentArgsRest(__VLS_6));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_5, typeof __VLS_7> & Record<string, unknown>) => void)({ ...{}, class: ("welcome-text"), });
+const __VLS_8 = __VLS_pickFunctionalComponentCtx(__VLS_5, __VLS_7)!;
+let __VLS_9!: __VLS_NormalizeEmits<typeof __VLS_8.emit>;
+{
+const __VLS_10 = ({} as __VLS_IntrinsicElements)["h1"];
+const __VLS_11 = __VLS_elementAsFunctionalComponent(__VLS_10);
+({} as __VLS_IntrinsicElements).h1;
+({} as __VLS_IntrinsicElements).h1;
+const __VLS_12 = __VLS_11({ ...{}, class: ("pretty-font"), }, ...__VLS_functionalComponentArgsRest(__VLS_11));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_10, typeof __VLS_12> & Record<string, unknown>) => void)({ ...{}, class: ("pretty-font"), });
+const __VLS_13 = __VLS_pickFunctionalComponentCtx(__VLS_10, __VLS_12)!;
+let __VLS_14!: __VLS_NormalizeEmits<typeof __VLS_13.emit>;
+(__VLS_13.slots!).default;
+}
+{
+const __VLS_15 = ({} as __VLS_IntrinsicElements)["p"];
+const __VLS_16 = __VLS_elementAsFunctionalComponent(__VLS_15);
+({} as __VLS_IntrinsicElements).p;
+({} as __VLS_IntrinsicElements).p;
+const __VLS_17 = __VLS_16({ ...{}, }, ...__VLS_functionalComponentArgsRest(__VLS_16));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_15, typeof __VLS_17> & Record<string, unknown>) => void)({ ...{}, });
+const __VLS_18 = __VLS_pickFunctionalComponentCtx(__VLS_15, __VLS_17)!;
+let __VLS_19!: __VLS_NormalizeEmits<typeof __VLS_18.emit>;
+(__VLS_18.slots!).default;
+}
+{
+const __VLS_20 = ({} as __VLS_IntrinsicElements)["button"];
+const __VLS_21 = __VLS_elementAsFunctionalComponent(__VLS_20);
+({} as __VLS_IntrinsicElements).button;
+({} as __VLS_IntrinsicElements).button;
+const __VLS_22 = __VLS_21({ ...{ onClick: {} as any, }, }, ...__VLS_functionalComponentArgsRest(__VLS_21));
+({} as (props: __VLS_FunctionalComponentProps<typeof __VLS_20, typeof __VLS_22> & Record<string, unknown>) => void)({ ...{ onClick: {} as any, }, });
+const __VLS_23 = __VLS_pickFunctionalComponentCtx(__VLS_20, __VLS_22)!;
+let __VLS_24!: __VLS_NormalizeEmits<typeof __VLS_23.emit>;
+let __VLS_25 = { 'click': __VLS_pickEvent(__VLS_24['click'], ({} as __VLS_FunctionalComponentProps<typeof __VLS_21, typeof __VLS_22>).onClick) };
+__VLS_25 = {
+click: (__VLS_ctx.navigateToAccounts)
+};
+(__VLS_23.slots!).default;
+}
+(__VLS_8.slots!).default;
+}
+(__VLS_3.slots!).default;
+}
+if (typeof __VLS_styleScopedClasses === 'object' && !Array.isArray(__VLS_styleScopedClasses)) {
+__VLS_styleScopedClasses["home-page"];
+__VLS_styleScopedClasses["welcome-text"];
+__VLS_styleScopedClasses["pretty-font"];
+}
+var __VLS_slots!: {};
+// @ts-ignore
+[navigateToAccounts,];
+return __VLS_slots;
+}
