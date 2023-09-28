a95ea197f328c85f95cc1738beda04444270140e
sarwari-mallela <123460981+sarwari-mallela@users.noreply.github.com>
2023-09-28 14:53:00 +0200

    Made a new file for the UAT environment, and added country as a column in the accounts page.

--- src/components/AppAccounts.vue
+++ src/components/AppAccounts.vue
@@ -27,6 +27,7 @@
                 <th scope="col">Account Number</th>
                 <th scope="col">Account Balance</th>
                 <th scope="col">Account Currency</th>
+                <th scope="col">Account Country</th>
                 <th scope="col">Account Status</th>
                 <th scope="col">Actions</th>
               </tr>
@@ -37,6 +38,7 @@
                 <td>{{ account.account_number }}</td>
                 <td>{{ account.balance }}</td>
                 <td>{{ account.currency }}</td>
+                <td>{{ account.country }}</td>
                 <td>
                   <span
                     v-if="account.status == 'Active'"
